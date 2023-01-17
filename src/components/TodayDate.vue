b6c3263be5d94171d157153d6db8c3b7839f65d8
Jervi <gacembekhira@gmail.com>
2023-01-17 02:54:29 +0100

    styled clock and made it functional

--- src/components/TodayDate.vue
+++ src/components/TodayDate.vue
@@ -1,15 +1,42 @@
 <template>
   <div class="today-date">
-    <span class="date">01 December 2022</span>
-    <span class="time">15:30</span>
+    <span class="date">{{date}}</span>
+    <span class="time">{{time}}<small>:{{second}}</small></span>
+    
   </div>
 </template>
 
 <script>
 export default {
+  
   name: 'TodayDate',
-  props: {
-    
+  data() {
+    return {
+      interval: null,
+      time: null,
+      date: null,
+      second: null
+    }
+  },
+  beforeDestroy() {
+    // prevent memory leak
+    clearInterval(this.interval)
+  },
+  created() {
+    // update the time every second
+    this.interval = setInterval(() => {
+      this.time = Intl.DateTimeFormat('fr-FR', {
+        hour: 'numeric',
+        minute: 'numeric'
+      }).format()
+      this.date = Intl.DateTimeFormat('fr-FR', {
+        day: '2-digit',
+        month: 'long'
+      }).format()
+      this.second = ("0" + Intl.DateTimeFormat('fr-FR', {
+        second: 'numeric'
+      }).format()).slice(-2);
+    }, 1000)
   }
 }
 </script>
@@ -18,17 +45,22 @@ export default {
 <style scoped lang="scss">
 .today-date {
   display: flex;
+  align-items: flex-end;
+  align-content: flex-end;
   gap: 1rem;
-  align-items: center;
   .date {
     font-weight: 700;
     font-size: 30px;
     color: #000000;
+    text-transform: capitalize;
   }
   .time {
     font-weight: 300;
     font-size: 25px;
     color: #000000;
+    small {
+      font-size: 19px;
+    }
   }
 }
 </style>
